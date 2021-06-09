d8c336eb3de8e1e5b50e2ef40e70049c118dba0b
PesoaGaston <pesoagaston@MacBook-Pro-de-PesoaGaston.local>
2021-06-08 21:19:50 -0300

    primer commit

---
+++ src/utilidades/UBean.java
@@ -0,0 +1,19 @@
+package utilidades;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
+public class UBean {
+	
+	public static ArrayList<Field> obtenerAtributos(Object obj) {
+		return new ArrayList<Field>();
+	}
+
+	public static void ejecutarSet(Object o, String att, Object valor) {
+		
+	}
+	
+	public static Object ejecutarGet(Object o, String att) {
+		return new Object();
+	}
+}
